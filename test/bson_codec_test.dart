a2a6b9a98cec0574e7fc2429965c89888c82788f
Giorgio Franceschetti <giorgiofran@gmail.com>
2023-11-11 16:01:32 +0100

    + 1 test

--- test/bson_codec_test.dart
+++ test/bson_codec_test.dart
@@ -4,6 +4,13 @@ import 'package:test/test.dart';
 // Example on how to use BSON to serialize-deserialize
 void main() {
   group('Bson Codec', () {
+    test('Empty Document', () {
+      final simpleMap = {};
+
+      final bson = BsonCodec.serialize(simpleMap);
+      final map = BsonCodec.deserialize(bson);
+      expect(map, simpleMap);
+    });
     test('Empty List', () {
       final simpleMap = {'emptyList': []};
 
